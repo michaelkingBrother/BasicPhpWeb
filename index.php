46e4ed1c80793fe00fe5026af55d988fa9f8e406
michaelkingBrother <ht.hophuoctrung@gmail.com>
2022-04-04 10:44:57 +0700

    make File Done

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php echo readfile("/html/header.html")?>
+<?php echo readfile("/html/body.html")?>
+<?php echo readfile("/html/footer.html")?>
+<p>Copyright © HoPhuocTrung from Bigbrothers.top <?php echo date("Y"); ?></p>
